f9bfc0346f025d8f8c3b30293f586be7a5cd4cc5
khameeteman <73279381+khameeteman@users.noreply.github.com>
2024-03-19 12:00:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 # Welcome to VIKTOR!
-#
-# Get started by running the following command in the terminal below:
-#   viktor-cli create-app --app-type editor
+# 
+# First time using VIKTOR?
+#   Keep this window open while we are preparing your
+#   codespace. You can now navigate back to your VIKTOR
+#   environment to complete the onboarding process.
