9878986cad7376df00ded79a94e1701795c18c67
PDA-structure <paulo@pda-structure.co.uk>
2024-10-25 00:11:08 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-# Welcome to VIKTOR!
-#
-# First time using VIKTOR?
-#   Keep this window open while we are activating your account.
-#
-#   You will see the following message in the window below when your Codespace has finished loading:
-#  
-#       The system check has been completed with the following conclusion:
-#
-#       V Your system is ready to use VIKTOR with isolation mode 'venv'
-#
-#   After that return to your VIKTOR environment where you will learn how to create your first app!
-#   Keep this Codespace open as this is the place where you will write your code.
+import viktor as vkt
+
+
+class Parametrization(vkt.Parametrization):
+    pass # Welcome to VIKTOR! You can add your input fields here. Happy Coding!
+
+
+class Controller(vkt.Controller):
+    parametrization = Parametrization
